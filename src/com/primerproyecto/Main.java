8a59967395cd3c70fdf0d5e31d4b3bf58f220101
Carlos de Tena Bellmont <carlostena@centroafuera.es>
2020-06-16 01:38:54 +0200

    Initial commit

---
+++ src/com/primerproyecto/Main.java
@@ -0,0 +1,10 @@
+package com.primerproyecto;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hola, esta es mi primera línea de código");
+        System.out.println("Esta es mi segunda línea de código");
+        System.out.println("Me despido con mi tercera línea de código");
+    }
+}
